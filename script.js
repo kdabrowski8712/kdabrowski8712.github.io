67efb868aca455a7a71a0323ae7ea1d7e3ba264c
k.dabrowski8712 <k.dabrowski8712@gmail.com>
2019-03-19 21:36:08 +0100

    60

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://obscure-sea-32382.herokuapp.com/v1/tasks/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'http://obscure-sea-32382.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
